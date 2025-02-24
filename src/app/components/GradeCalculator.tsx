b8e271e16cbdd34bad3ff49cc4777cf332a5e7d4
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-24 18:40:33 +0500

    if else condition

--- src/app/components/GradeCalculator.tsx
+++ src/app/components/GradeCalculator.tsx
@@ -14,7 +14,14 @@ export default function GradeCalculator() {
         const totalMarks = 1100;
         const percentage = (ObtainMarks / totalMarks) * 100;
         const rounded = percentage.toFixed(1);
-    }
+
+        if (ObtainMarks >= 1000) return { grade: "A+", percentage: rounded};
+        if (ObtainMarks >= 850) return { grade: "A", percentage: rounded};
+        if (ObtainMarks >= 750) return { grade: "B+", percentage: rounded};
+        if (ObtainMarks >= 600) return { grade: "B", percentage: rounded};
+        if (ObtainMarks >= 450) return { grade: "C", percentage: rounded};
+        return {grade: "Failed", percentage: rounded}
+    };
 
 }
 
