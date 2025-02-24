53c1b2ec88d962b116e5aa85ec02fd1d9e0db20f
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-24 18:26:55 +0500

    Declare (state)

--- src/app/components/GradeCalculator.tsx
+++ src/app/components/GradeCalculator.tsx
@@ -1,6 +1,13 @@
 'use client'
+import useState from "react";
 
 interface GradeResult {
     grade: string,
     percentage: string
 }
+
+export default function GradeCalculator() {
+    const [marks, setMarks] = useState('');
+    const [resul, setResult] = useState<GradeResult | null>(null);
+}
+
